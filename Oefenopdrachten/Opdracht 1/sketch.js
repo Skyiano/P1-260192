d5499c2d5fdba9c49b815d89e8a24284fd4af6e4
Skyiano <gamer.1998@icloud.com>
2026-09-08 12:26:46 +0200

    mondiaan gemaakt

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,107 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 800);
 }
 
 function draw() {
   background(220);
+stroke("black");
+strokeWeight(4);
+fill("white");
+rect(10, 10, 900, 550);
+
+stroke("black");
+strokeWeight(4);
+fill("white");
+rect(910, 10, 75, 550);
+
+stroke("black");
+strokeWeight(4);
+fill("yellow");
+rect(210, 70, 200, 175)
+
+stroke("black");
+strokeWeight(4);
+line(10, 70, 820, 70);
+
+stroke("black");
+strokeWeight(4);
+fill("black");
+rect(10, 70, 200, 175);
+
+stroke("black");
+strokeWeight(4);
+fill("blue");
+rect(400, 200, 100, 300);
+
+stroke("black");
+strokeWeight(4);
+fill("red");
+rect(500, 70, 320, 175);
+
+stroke("black");
+strokeWeight(4);
+line(820, 10, 820, 560);
+
+stroke("black");
+strokeWeight(4);
+fill("blue"); 
+rect(910, 10, 75, 235);
+
+stroke("black");
+strokeWeight(4);
+fill("black");
+rect(910, 245, 75, 315);
+
+stroke("black");
+strokeWeight(4);
+fill("red");
+rect(10, 245, 290, 315);
+
+stroke("black");
+strokeWeight(4);
+line(10, 245, 985, 245);
+
+stroke("black");
+strokeWeight(4);
+fill("blue");
+rect(10, 10, 290, 60);
+
+stroke("black");
+strokeWeight(4);
+line(400, 560, 400, 10);
+
+stroke("black");
+strokeWeight(4);
+fill("black");
+rect(400, 10, 100, 235);
+
+stroke("black");
+strokeWeight(4);
+line(500, 560, 500, 10);
+
+
+stroke("black");
+strokeWeight(4);
+line( 820, 150, 910, 150);
+
+stroke("black");
+strokeWeight(4);
+fill("yellow");
+rect(820, 10, 90, 140);
+
+stroke("black");
+strokeWeight(4);
+line(10, 500, 910,500);
+
+stroke("black");
+strokeWeight(4);
+fill("red")
+rect(820, 245, 90, 255)
+
+stroke("black");
+strokeWeight(4);
+fill("yellow");
+rect(10, 500, 900, 60);
+
+
 }
