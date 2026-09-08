6e2f10853573a3f0caad67d938af35a5061f2acd
Skyiano <gamer.1998@icloud.com>
2026-09-08 15:31:14 +0200

    added bunch of stuff

--- week 1 - hello world/sketch.js
+++ week 1 - hello world/sketch.js
@@ -8,12 +8,11 @@ function draw() {
 
   //writes name
   textSize(24);
-  fill("black");
   text("1. skyhoek", 10, 50);
 
 textSize(24);
 fill("black");
-text("2.", 10, 100)
+text("2.", 10, 100);
 
 //makes red square flag
   noStroke(0);
@@ -28,12 +27,12 @@ text("2.", 10, 100)
 //make blue square
 noStroke(0);
 fill("blue");
-rect(10, 220, 200, 50)
+rect(10, 220, 200, 50);
 
 //schaakbord
 textSize(24);
 fill("black");
-text("3.", 10, 300)
+text("3.", 10, 300);
 stroke("black");
 strokeWeight(4);
 fill("white");
@@ -51,8 +50,31 @@ rect(110, 450, 50, 50);
 textSize(24);
 fill("black");
 text("4.", 10, 550);
-stroke("black")
-triangle(100, 700, 100, 200, 100, 200)
+stroke("black");
+strokeWeight(4);
+fill("gray");
+triangle(10, 600, 95, 550, 175, 600,);
+rect(15, 600, 155, 125);
 
+//verkeerslicht
+textSize(24);
+text("5.", 300, 50);
 
+noStroke(0);
+fill("black");
+rect(300, 60, 100, 200);
+noStroke(0);
+fill("red");
+circle(350, 100, 50);
+noStroke(0);
+fill("yellow");
+circle(350, 160, 50);
+noStroke(0);
+fill("green");
+circle(350, 220, 50);
+noStroke(0);
+fill("black");
+rect(330, 250, 40, 50);
+
+//dobbelsteen
 }
