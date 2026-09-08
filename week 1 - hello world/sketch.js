9e1661cc6eb52c037946eeda8b945b05741947a8
Skyiano <gamer.1998@icloud.com>
2026-09-08 13:27:50 +0200

    Update sketch.js

--- week 1 - hello world/sketch.js
+++ week 1 - hello world/sketch.js
@@ -1,9 +1,16 @@
 function setup() {
-  createCanvas(611, 611);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
-  text("1 Sky Hoek", 50, 50);
-  rect(100, 50, 200, 150);
+  background("white");
+fill("gray");
+noStroke(0);
+  rect(100, 60, 200, 500);
+  rect(150, 500, 100, 200);
+
+  fill("red");
+  noStroke(0);
+  circle(200, 200, 100);
+  
 }
