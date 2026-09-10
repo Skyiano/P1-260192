1b3ebb6fd3dbb207fe0b7a4f9dfbaa1c168e160a
Skyiano <gamer.1998@icloud.com>
2026-09-10 12:01:27 +0200

    added lots

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,19 @@
+let x = 100;
+let greeting = "hallo wereld";
+let a = 20;
+let b = 10;
+let optellen = a + b;
+
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  textSize(24);
+  text(x, 20, 20);
+  text(greeting, 20, 60);
+  
+
 }
