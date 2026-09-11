acb41875475cb6d68f57ee03dc3f74a3dfee80b4
Skyiano <gamer.1998@icloud.com>
2026-09-11 09:59:48 +0200

    variabelen veranderd

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,13 +1,19 @@
-let x = 100;
-let greeting = "hallo wereld";
-let a = 20;
+let x = 100;{
+// zet x naar waarde 100
+}
+let greeting = "hallo wereld";{
+//zet greeting naar text waarde hallo wereld
+}
+let a = 20;{
 let b = 10;
+//geeft een waarde aan a en b
 let optellen = a + b;
 let aftrekken = a - b;
 let multiply = a * b;
 let delen = a / b;
+//maakt sommen met waarde a en b
 let y = 80;
-
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -18,11 +24,4 @@ function draw() {
   textSize(24);
   text(x, 20, 20);
   text(greeting, 20, 60);
-  text(optellen, 20, 100);
-  text(aftrekken, 20, 140);
-  text(multiply, 20, 180);
-  text(delen, 20, 220);
- 
-  
-
 }
